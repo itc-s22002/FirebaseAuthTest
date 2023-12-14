a21c665114b4437c1074ceaec1183afa92da5b6d
s22002 <s22002@std.it-college.ac.jp>
2023-12-15 00:39:06 +0900

    loginページの作成コンソールログで成功が確認できた

---
+++ pages/login.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import auth from '../FirebaseConfig'; // firebase.js のパスを指定
+import { useRouter } from 'next/router';
+
+const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const router = useRouter();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            // FirebaseのsignInWithEmailAndPasswordメソッドを使用してログイン
+            await signInWithEmailAndPassword(auth, email, password);
+            // router.push('/dashboard'); // ログイン成功後、ダッシュボードページにリダイレクト
+            console.log("Ok")
+        } catch (error) {
+            console.error('Login error:', error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <form onSubmit={handleLogin}>
+                <div>
+                    <label>
+                        Email:
+                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Password:
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </label>
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default LoginPage;
